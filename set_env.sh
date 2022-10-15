9f06e14fff8acdbe36341ac80d589cb9834903e4
Teehdev <atiladeoyetunji@gmail.com>
2022-10-15 17:03:06 +0100

    initial commit

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=atilade12
+export POSTGRES_PASSWORD=ATilade12..
+export POSTGRES_HOST=database-2.c480b2qthc4n.us-east-1.rds.amazonaws.com
+#database-1.c480b2qthc4n.us-east-1.rds.amazonaws.com 
+#postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mybucket1200798
 export AWS_REGION=us-east-1
